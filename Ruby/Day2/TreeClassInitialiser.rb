d5351856d34139d2f524cca4aadb434300cef487
Tony Lawrence <code@tonylawrence.com>
2012-05-30 21:51:25 +0100

    Added task

--- Ruby/Day2/TreeClassInitialiser.rb
+++ Ruby/Day2/TreeClassInitialiser.rb
@@ -1,3 +1,8 @@
+# The Tree class was interesting, but it did not allow you to specify a new tree with a clean user interface.
+# Let the initializer accept a nested structure of hashes. 
+# You should be able to specify a tree like this: 
+# {'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } }.
+
 class Tree
   attr_accessor :children, :node_name
 
