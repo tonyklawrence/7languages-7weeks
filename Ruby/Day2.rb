ee0a74653d67e9c5c2376d420e629926c35a075c
Tony Lawrence <code@tonylawrence.com>
2012-05-30 21:46:06 +0100

    Added slicing with each

--- Ruby/Day2.rb
+++ Ruby/Day2.rb
@@ -17,3 +17,15 @@ p hash.to_a.flatten
 # Can you translate arrays to hashes? (yes, but why the *?)
 array = hash.to_a.flatten
 p Hash[*array]
+
+# Can you iterate through a hash? (sure)
+hash.each { |x| p x }
+
+# Print the contents of an array of sixteen numbers, four numbers at a time, using just each.
+# Only done easily by re-referencing the original list
+array = (1..16).to_a
+array.each { |x| p array[x-1, 4] if x % 4 == 1}
+
+# Now, do the same with each_slice in Enumerable.
+(1..16).each_slice(4) { |slice| p slice }
+
