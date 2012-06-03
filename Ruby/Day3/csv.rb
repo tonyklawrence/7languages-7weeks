e3569bff80f9355921d83543631d20a0992445b8
Tony Lawrence <code@tonylawrence.com>
2012-06-03 13:23:15 +0100

    Created csv file and placed original ruby code

--- Ruby/Day3/csv.rb
+++ Ruby/Day3/csv.rb
@@ -1,9 +1,53 @@
 # Modify the CSV application to support an each method to return a CsvRow object. 
 # Use method_missing on that CsvRow to return the value for the column for a given heading.
+
 # For example, for the file:
 # one, two
 # lions, tigers
+
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+      
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+    def initialize
+      read
+    end 
+  end
+end
+
+class RubyCsv # no inheritance! You can mix it in include ActsAsCsv
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
 # allow an API that works like this:
 # csv = RubyCsv.new
 # csv.each {|row| puts row.one}
 # This should print "lions".
+
+csv = RubyCsv.new
+csv.each {|row| puts row.one}
