aa9f07761c87a34de81ec72b6a612c8d1619e8be
Tony Lawrence <code@tonylawrence.com>
2012-06-18 22:05:09 +0100

    Added board to print

--- Scala/Day1/TicTacToe.scala
+++ Scala/Day1/TicTacToe.scala
@@ -1,4 +1,16 @@
+package Day1
+
+sealed abstract class Square
+case class O() extends Square
+case class X() extends Square
+case class ?() extends Square
+
+class Board {
+  val board = (?,?,?) :: (?,?,?) :: (?,?,?) :: Nil
+
+  override def toString = "%s\n%s\n%s".format(board(0), board(1), board(2))
+}
 
 object TicTacToe extends App {
-  println("Yeah")
+  println(new Board)
 }
